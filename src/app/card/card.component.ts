91117cf838f5bfa99d1cca117e69573d0b8bebb3
Náder W. S. Sírio <nadersirio@gmail.com>
2023-12-14 20:32:47 -0300

    add: ngFor

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent implements OnInit {
+export class CardComponent implements OnInit, OnChanges {
   products:string[] = []
-
+  placeholder:string = "Insira um item a lista!"
   constructor() {
     this.products = [
       "mouse",
@@ -16,6 +16,14 @@ export class CardComponent implements OnInit {
       "headset"
     ]
   }
-
+  ngOnChanges(): void {
+    this.placeholder = ""
+  }
   ngOnInit(): void {}
+  adicionar(){
+    this.products.push(this.placeholder)
+  }
+  remove(){
+    this.products.pop()
+  }
 }
